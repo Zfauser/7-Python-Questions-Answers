1d9971dff957218f685b61f3e3bcfc731b679f85
Zach <zach@fauser.net>
2021-02-25 09:57:04 -0500

    Create Question 2.py

---
+++ Question 2.py
@@ -0,0 +1,6 @@
+markone = int(input("Please enter your first mark: \n"))
+marktwo = int(input("Please enter your second mark: \n"))
+markthree = int(input("Please enter your third mark: \n"))
+markfour = int(input("Please enter your forth mark: \n"))
+totalmark = int((markone + marktwo + markthree + markfour) /4)
+print("Your mark is", totalmark)
