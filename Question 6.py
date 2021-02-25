abced5a33ea0a03e285bd5093c678123e47f8f97
Zach <zach@fauser.net>
2021-02-25 11:00:18 -0500

    Create Question 6.py

---
+++ Question 6.py
@@ -0,0 +1,7 @@
+while True:
+    password = input("Please type your password: \n")
+    if password=="Zach":
+        print("login successful")
+        break
+    else:
+        print("incorrect login please try again.")
